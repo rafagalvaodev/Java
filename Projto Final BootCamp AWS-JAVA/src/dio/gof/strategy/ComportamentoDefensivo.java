b5908bce723719cf03cdb9bc06a974591a2d7fb5
RafaGalvaoDev <rafagalvaonull@gmail.com>
2023-08-17 11:15:23 -0300

    Padrão ComportamentoDefensivo

---
+++ Projto Final BootCamp AWS-JAVA/src/dio/gof/strategy/ComportamentoDefensivo.java
@@ -0,0 +1,13 @@
+package dio.gof.strategy;
+
+public class ComportamentoDefensivo implements Comportamentos {
+
+	@Override
+	public void mover() {
+		// TODO Auto-generated method stub
+		System.out.println("Se protegendo");
+	}
+
+	
+	
+}
