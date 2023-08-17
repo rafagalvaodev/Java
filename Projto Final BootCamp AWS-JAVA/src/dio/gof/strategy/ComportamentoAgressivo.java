eeab9d2f6066f98264cbad9f1959fcbfe99a08b9
RafaGalvaoDev <rafagalvaonull@gmail.com>
2023-08-17 11:15:05 -0300

    Padrão ComportamentoAgressivo

---
+++ Projto Final BootCamp AWS-JAVA/src/dio/gof/strategy/ComportamentoAgressivo.java
@@ -0,0 +1,15 @@
+package dio.gof.strategy;
+
+public class ComportamentoAgressivo implements Comportamentos {
+
+
+	@Override
+	public void mover() {
+		// TODO Auto-generated method stub
+		System.out.println("Atacando");
+		
+	}
+
+	
+	
+}
