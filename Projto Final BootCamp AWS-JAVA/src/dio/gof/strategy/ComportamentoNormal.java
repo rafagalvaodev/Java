9dbbaafbb427d54d5e05e8cf33659271a0056a5d
RafaGalvaoDev <rafagalvaonull@gmail.com>
2023-08-17 11:15:43 -0300

    Padrão ComportamentoNormal

---
+++ Projto Final BootCamp AWS-JAVA/src/dio/gof/strategy/ComportamentoNormal.java
@@ -0,0 +1,13 @@
+package dio.gof.strategy;
+
+public class ComportamentoNormal implements Comportamentos {
+
+	@Override
+	public void mover() {
+		// TODO Auto-generated method stub
+		System.out.println("Se movendo");
+	}
+
+	
+	
+}
