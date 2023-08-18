0768ae3b095abe5514f5e535491ae9aca9bc3ab6
RafaGalvaoDev <rafagalvaonull@gmail.com>
2023-08-18 18:50:04 -0300

    Classe User

---
+++ Santander_dev_week_2023/src/main/java/me/dio/Santander_dev_week_2023/domain/models/User.java
@@ -0,0 +1,73 @@
+package me.dio.Santander_dev_week_2023.domain.models;
+
+import java.util.List;
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+
+@Entity(name = "tb_user")
+public class User {
+
+	@Id
+	@GeneratedValue(strategy =  GenerationType.IDENTITY)
+	private Long id;
+	
+	
+	private String name;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private Account account;
+	
+	@OneToOne(cascade = CascadeType.ALL)
+	private	Card card;
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER )
+	private List <Feature> feature;
+	
+	@OneToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+	private	List <News> news;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return name;
+	}
+	public void setNome(String nome) {
+		this.name = nome;
+	}
+	public Account getAccount() {
+		return account;
+	}
+	public void setAccount(Account account) {
+		this.account = account;
+	}
+	public Card getCard() {
+		return card;
+	}
+	public void setCard(Card card) {
+		this.card = card;
+	}
+	public List<Feature> getFeature() {
+		return feature;
+	}
+	public void setFeature(List<Feature> feature) {
+		this.feature = feature;
+	}
+	public List<News> getNews() {
+		return news;
+	}
+	public void setNews(List<News> news) {
+		this.news = news;
+	}
+}
