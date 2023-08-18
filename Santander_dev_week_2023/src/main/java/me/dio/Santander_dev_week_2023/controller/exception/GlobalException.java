567a39be49e9bb71bcc43963c8633cf8344ff0c4
RafaGalvaoDev <rafagalvaonull@gmail.com>
2023-08-18 18:56:55 -0300

    Classe GlobalException

---
+++ Santander_dev_week_2023/src/main/java/me/dio/Santander_dev_week_2023/controller/exception/GlobalException.java
@@ -0,0 +1,33 @@
+package me.dio.Santander_dev_week_2023.controller.exception;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.NoSuchElementException;
+
+@RestControllerAdvice
+public class GlobalException {
+
+    private final Logger logger  = LoggerFactory.getLogger(GlobalException.class);
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<String> handleBusinessException(IllegalArgumentException businessException) {
+        return new ResponseEntity<>(businessException.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
+    }
+
+    @ExceptionHandler(NoSuchElementException.class)
+    public ResponseEntity<String> handleNotFoundException(NoSuchElementException notFoundException) {
+        return new ResponseEntity<>("Id não encontrado", HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(Throwable.class)
+    public ResponseEntity<String> handleUnexpectedException(Throwable unexpectedException) {
+        var message = "Erro inesperado no servidor, veja os logs";
+        logger.error(message, unexpectedException);
+        return new ResponseEntity<>(message, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+}
