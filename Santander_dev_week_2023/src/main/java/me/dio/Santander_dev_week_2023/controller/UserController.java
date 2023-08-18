61aa19a8d1c88f6cc4c120f6b697d7eed1fc1fc5
RafaGalvaoDev <rafagalvaonull@gmail.com>
2023-08-18 18:56:19 -0300

    Classe UserController

---
+++ Santander_dev_week_2023/src/main/java/me/dio/Santander_dev_week_2023/controller/UserController.java
@@ -0,0 +1,41 @@
+package me.dio.Santander_dev_week_2023.controller;
+
+import java.net.URI;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import me.dio.Santander_dev_week_2023.domain.models.User;
+import me.dio.Santander_dev_week_2023.services.UserService;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+	private final UserService userService;
+
+	public UserController(UserService userService) {
+		this.userService = userService;
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		var user = userService.findById(id);
+		return ResponseEntity.ok(user);
+	}
+	
+	@PostMapping
+	public ResponseEntity<User> create(@RequestBody User user){
+		var userCreated = userService.create(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(userCreated.getId()).toUri();
+		return ResponseEntity.created(location).body(userCreated);
+	}
+	
+	
+}
