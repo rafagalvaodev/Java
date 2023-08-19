1ff018dfc5fc8b8efa2a0239fe6b80e1186e7163
RafaGalvaoDev <rafagalvaonull@gmail.com>
2023-08-19 14:55:23 -0300

    Classe Retorno

---
+++ Candidados/selecao_cadidatos/src/selecao/Returno.java
@@ -0,0 +1,41 @@
+package selecao;
+
+import java.util.Scanner;
+
+public class Returno {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Criação do Scanner para receber as informações do usuario
+		//Scanner input = new Scanner(System.in);
+		
+		
+		
+		//System.out.print("Digite o Salario Pretendido: ");
+		// Recebe o valor do usuario
+		//double salarioPretendido = input.nextDouble();
+		
+		// Criando uma instancia da classe processo
+		//Processo processo = new Processo();
+		
+		
+		// Analisando o salario pretendido com o salario base atraves deste metodo da classe processo
+		//processo.analisandoCandidato(salarioPretendido);
+		
+		// Fecha a entrada de informações passada pelo usuario
+		//input.close();
+		
+		// -----------------------------------------------------------------------------------------------------------------
+		
+		SelecaoCincoCandidatos selecionando = new SelecaoCincoCandidatos();
+		
+		selecionando.selecionaCinco();
+		
+		
+		
+		
+		
+		
+	}
+
+}
