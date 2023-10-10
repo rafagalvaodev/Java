efe75b99c362aa8847265a408045eecece53043e
RafaGalvaoDev <rafagalvaonull@gmail.com>
2023-10-10 18:52:53 -0300

    Interface Publicaçao

---
+++ ExercicioPessoa/src/exerciciopessoa/Publicacao.java
@@ -0,0 +1,9 @@
+package exerciciopessoa;
+public interface Publicacao {
+    public void open();
+    public void close();
+    public void leaf_Through(int page);
+    public void next_Page();
+    public void back_Page();
+    
+}
