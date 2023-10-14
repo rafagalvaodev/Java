e8527e59ceeac40d93da3a5f71caf83bdcc852e4
RafaGalvaoDev <rafagalvaonull@gmail.com>
2023-10-14 10:53:36 -0300

    For

---
+++ For/src/pkgfor/For.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pkgfor;
+
+/**
+ *
+ * @author PC
+ */
+public class For {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int tabuada = 6;
+        for(int contador = 10; contador>=1; contador--){
+            System.out.println("A Tabuada de " + tabuada + " Vezes " + contador + " = " + tabuada * contador);
+        }
+    }
+    
+}
