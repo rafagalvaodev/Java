65237b311e04090b53230a650b7ec9bb10f1fcd2
RafaGalvaoDev <rafagalvaonull@gmail.com>
2023-10-17 22:17:19 -0300

    While 02

---
+++ while02/src/while02/While02.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package while02;
+
+/**
+ *
+ * @author PC
+ */
+public class While02 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int cambalhota = 1;
+        do {
+            cambalhota ++;
+            System.out.println("Do a barrel roll");
+        } while(cambalhota <=5);
+    }
+    
+}
