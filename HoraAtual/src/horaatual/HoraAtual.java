b3a43848708adf6c23db18b3aff0cf0fb557cb8c
RafaGalvaoDev <rafagalvaonull@gmail.com>
2023-10-21 16:36:24 -0300

    Hora Atual

---
+++ HoraAtual/src/horaatual/HoraAtual.java
@@ -0,0 +1,12 @@
+package horaatual;
+
+import java.util.Date;
+
+public class HoraAtual {
+    public static void main(String[] args) {
+        Date hora = new Date();
+        System.out.println("A Hora Atual é: ");
+        System.out.println(hora.toString());
+    }
+    
+}
