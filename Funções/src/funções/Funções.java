bd46ef3b1d8f912c2d7b41551b6bc83efd7461ba
RafaGalvaoDev <rafagalvaonull@gmail.com>
2023-10-23 18:18:21 -0300

    Funções

---
+++ Funções/src/funções/Funções.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package funções;
+
+/**
+ *
+ * @author PC
+ */
+public class Funções {
+
+    /**
+     * @param args the command line arguments
+     */
+    
+    static void soma(int a, int b){
+        int soma = a + b;
+        System.out.println(soma);
+    }
+    
+   
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+         soma(3,6);
+    }
+    
+}
