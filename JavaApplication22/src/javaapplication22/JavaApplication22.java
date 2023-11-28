410872680f4625c6f4c2050d8bae926fb42b51bb
RafaGalvaoDev <rafagalvaonull@gmail.com>
2023-11-28 09:12:45 -0300

    Teste de Código

---
+++ JavaApplication22/src/javaapplication22/JavaApplication22.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication22;
+
+/**
+ *
+ * @author PC
+ */
+public class JavaApplication22 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       int numero = 10;
+       numero--;
+        System.out.println(numero);
+    }
+    
+}
