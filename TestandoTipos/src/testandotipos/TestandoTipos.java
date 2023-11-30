324492e9a11584764f7c7fc0e3a58d192395c635
RafaGalvaoDev <rafagalvaonull@gmail.com>
2023-11-30 09:59:59 -0300

    Testando Tipos

---
+++ TestandoTipos/src/testandotipos/TestandoTipos.java
@@ -0,0 +1,9 @@
+package testandotipos;
+public class TestandoTipos {
+    public static void main(String[] args) {
+        int idade = 30;
+        String valor = Integer.toString(idade);
+        System.out.printf("%s", valor);
+    }
+    
+}
