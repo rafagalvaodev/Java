6814cab9b2ef72a976648f3ab87b5ea4c50ed215
RafaGalvaoDev <rafagalvaonull@gmail.com>
2023-12-01 05:51:08 -0300

    Operador Ternario

---
+++ OperadorTernario/src/operadorternario/OperadorTernario.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package operadorternario;
+
+/**
+ *
+ * @author PC
+ */
+public class OperadorTernario {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int n1, n2, n3;
+        n1 = 4;
+        n2 = 7;
+        n3 = 12;
+        boolean r;
+        r = (n1<n2 && n2<n3) ? true:false;
+        System.out.println("Resultado " + r);
+        
+                
+              
+    }
+    
+}
