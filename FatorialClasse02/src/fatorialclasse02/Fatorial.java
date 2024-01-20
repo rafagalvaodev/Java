af691bb9d1f9f1bfaef6a41be88cdf56c5d715be
RafaGalvaoDev <rafagalvaonull@gmail.com>
2024-01-20 19:28:14 -0300

    Fatorial Classe 02

---
+++ FatorialClasse02/src/fatorialclasse02/Fatorial.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package fatorialclasse02;
+
+/**
+ *
+ * @author PC
+ */
+public class Fatorial {
+    private int numero = 0;
+    private int fatorial = 1;
+    private String formula = "";
+    
+    public void valor(int num){
+        numero = num;
+        int fac = 1;
+        String soma = "";
+        for(int contador = num; contador > 1; contador--){
+            fac *= contador;
+            soma += contador + " X ";
+        }
+        soma += "1 = ";
+        fatorial = fac;
+        formula = soma;
+                
+    }
+    
+    public int mostraFactorial(){
+        return fatorial;
+    }
+    
+    public String mostraFormula(){
+        return formula;
+    }
+    
+}
