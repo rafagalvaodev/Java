5259f6608475a7637abfcaa5b585165e4d0fb60a
RafaGalvaDev <rafagalvaonull@gmail.com>
2025-08-18 11:47:26 -0300

    Game Seeds

--- Projetos/dslist/src/main/java/com/projectspring/dslist/entities/Game.java
+++ Projetos/dslist/src/main/java/com/projectspring/dslist/entities/Game.java
@@ -17,10 +17,14 @@ public class Game {
     private Integer year;
 
     private String genre;
-    private String platform;
+    private String platforms;
     private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     public Game(){}
@@ -30,7 +34,7 @@ public class Game {
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.platform = platform;
+        this.platforms = platform;
         this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
@@ -69,12 +73,12 @@ public class Game {
         this.genre = genre;
     }
 
-    public String getPlatform() {
-        return platform;
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlatform(String platform) {
-        this.platform = platform;
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
 
     public Double getScore() {
@@ -128,7 +132,7 @@ public class Game {
                 ", title='" + title + '\'' +
                 ", year=" + year +
                 ", genre='" + genre + '\'' +
-                ", platform='" + platform + '\'' +
+                ", platform='" + platforms + '\'' +
                 ", score=" + score +
                 ", imgURL='" + imgUrl + '\'' +
                 ", shortDescription='" + shortDescription + '\'' +
