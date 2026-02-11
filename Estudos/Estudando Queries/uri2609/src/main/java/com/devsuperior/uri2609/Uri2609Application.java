4a02d944f8b3e0e2293db17423ab9665ee75e920
rafagalvaodev <rafagalvaonull@gmail.com>
2026-02-11 16:54:28 -0300

    :sparkles: Added second query's learn

---
+++ Estudos/Estudando Queries/uri2609/src/main/java/com/devsuperior/uri2609/Uri2609Application.java
@@ -0,0 +1,43 @@
+package com.devsuperior.uri2609;
+
+import com.devsuperior.uri2609.dto.CategorySumDTO;
+import com.devsuperior.uri2609.projections.CategorySumProjection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.devsuperior.uri2609.repositories.CategoryRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class Uri2609Application implements CommandLineRunner {
+
+	@Autowired
+	private CategoryRepository repository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Uri2609Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		List<CategorySumProjection> list = repository.seachOne();
+
+		List<CategorySumDTO> seacheOne = list.stream().map(CategorySumDTO::new).collect(Collectors.toList());
+		System.out.println("\n\nConsulta SQL RAIZ");
+		for (CategorySumDTO categorySumDTO: seacheOne){
+			System.out.println(categorySumDTO);
+		}
+
+
+		List<CategorySumDTO> seachTwo = repository.seachTwo();
+		System.out.println("\n\nConsulta SQL RAIZ");
+		for (CategorySumDTO categorySumDTO: seachTwo){
+			System.out.println(categorySumDTO);
+		}
+
+	}
+}
