a60337eb473158ebc1c788225f4b5c342c9854ae
rafagalvaodev <rafagalvaonull@gmail.com>
2026-02-23 11:35:32 -0300

    :sparkles: Added fifth query's learn

---
+++ Estudos/Estudando Queries/uri2737/src/main/java/com/devsuperior/uri2737/Uri2737Application.java
@@ -0,0 +1,41 @@
+package com.devsuperior.uri2737;
+
+import com.devsuperior.uri2737.dto.LawyerMinDTO;
+import com.devsuperior.uri2737.projections.LawyerMinProjection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.devsuperior.uri2737.repositories.LawyerRepository;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@SpringBootApplication
+public class Uri2737Application implements CommandLineRunner {
+
+	@Autowired
+	private LawyerRepository repository;
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Uri2737Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		List<LawyerMinProjection> list = repository.searchOne();
+
+		List<LawyerMinDTO> searchOne =
+				list
+						.stream()
+						.map(LawyerMinDTO::new)
+						.collect(Collectors.toList());
+
+		for (LawyerMinDTO lawyerMinDTO : searchOne){
+			System.out.println(lawyerMinDTO);
+		}
+
+	}
+}
